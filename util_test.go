f21525fa50aa41e2aa7885c1b2fe346ddd021e61
jst <janschlicht@gmail.com>
2014-03-29 11:03:34 +0100

    Add tests for Thumbnail.

--- util_test.go
+++ util_test.go
@@ -31,3 +31,30 @@ func Test_FromWidthLargeInt(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test_Thumbnail(t *testing.T) {
+	var thumbnailTests = []struct {
+		origWidth      int
+		origHeight     int
+		maxWidth       uint
+		maxHeight      uint
+		expectedWidth  uint
+		expectedHeight uint
+	}{
+		{5, 5, 10, 10, 5, 5},
+		{10, 10, 5, 5, 5, 5},
+		{10, 50, 10, 10, 2, 10},
+		{50, 10, 10, 10, 10, 2},
+		{50, 100, 60, 90, 45, 90},
+		{120, 100, 60, 90, 60, 50},
+		{200, 250, 200, 150, 120, 150},
+	}
+
+	for _, tt := range thumbnailTests {
+		rect := image.Rect(0, 0, tt.origWidth, tt.origHeight)
+		actualWidth, actualHeight := Thumbnail(rect, tt.maxWidth, tt.maxHeight)
+		if actualWidth != tt.expectedWidth || actualHeight != tt.expectedHeight {
+			t.Fail()
+		}
+	}
+}
